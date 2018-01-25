61adc8351f70bb1a760c8506dc87bf850a9033e4
shansilia <shansiliya@yandex.ru>
2018-01-25 23:29:07 +0300

    initial commit

---
+++ app/src/main/java/by/md/gornak/homework/MainActivity.java
@@ -0,0 +1,41 @@
+package by.md.gornak.homework;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+
+        ImageView avatar = findViewById(R.id.avatar);
+        Resources res = getResources();
+        Bitmap src = BitmapFactory.decodeResource(res, R.drawable.avatar);
+        src = Bitmap.createBitmap(
+                src,
+                0,
+                src.getHeight()/2 - src.getWidth()/2,
+                src.getWidth(),
+                src.getWidth()
+        );
+        RoundedBitmapDrawable dr =
+                RoundedBitmapDrawableFactory.create(res, src);
+        dr.setCornerRadius(dr.getMinimumHeight()*2);
+       // dr.setCircular(true);
+        avatar.setImageDrawable(dr);
+
+    }
+}
