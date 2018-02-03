8d8e6a2ba5c9f5c306e920937c1b1bd2a6fca415
shansilia <shansiliya@yandex.ru>
2018-02-03 17:43:05 +0300

    change older structure

--- app/src/main/java/by/md/gornak/homework/activity/ThemeActivity.java
+++ app/src/main/java/by/md/gornak/homework/activity/ThemeActivity.java
@@ -1,4 +1,4 @@
-package by.md.gornak.homework;
+package by.md.gornak.homework.activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+
+import by.md.gornak.homework.R;
 
 public class ThemeActivity extends AppCompatActivity {
 
